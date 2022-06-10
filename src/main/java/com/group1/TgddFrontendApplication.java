9125a10196cfbe5026f753631c420dc4fe8b0915
Nguyễn Minh Quân <99967044+miquanajoy@users.noreply.github.com>
2022-06-10 19:07:46 +0700

    first commit
    first commit

---
+++ src/main/java/com/group1/TgddFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.group1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgddFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgddFrontendApplication.class, args);
+	}
+
+}
