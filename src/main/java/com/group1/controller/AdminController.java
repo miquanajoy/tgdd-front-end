b0dd4cf222692fb9bf29a1e4d03459a90ff1f1b9
MaiThanhHoang <86399605+DragonKnightSamie@users.noreply.github.com>
2022-07-18 18:43:27 +0700

    Update front end project

--- src/main/java/com/group1/controller/AdminController.java
+++ src/main/java/com/group1/controller/AdminController.java
@@ -1,5 +1,33 @@
 package com.group1.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.group1.dto.PromotionsList;
+
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,15 +39,59 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/admin")
 public class AdminController {
 
-	@RequestMapping("/home")
-	public String home(Model model) {
-		RestTemplate restTemplate = new RestTemplate();
-		String fooResourceUrl = "http://localhost:8080/admin/home";
-		ResponseEntity<String> responseEntity
-				= restTemplate.getForEntity(fooResourceUrl, String.class);
-		model.addAttribute("text", responseEntity.getBody());
-		return "index";
+	@GetMapping("/promotions-management/view-promotions")
+	public ModelAndView showAllPromotions (ModelAndView model) 
+	{
 		
+		//Map<String, Object> map = new HashMap<String, Object>();
+		PromotionsList promote = new PromotionsList();
+		RestTemplate resttemp = new RestTemplate();
+		String resourceUrl = "http://localhost:8080/admin/promotions-management/view-promotions";
+		ResponseEntity<String> response = resttemp.getForEntity(resourceUrl, String.class);
+		String responseBody = "";
+		responseBody = response.getBody();
+		//if(!responseBody.isBlank() || responseBody != null) System.out.println("Map object"+responseBody.toString() );
+		//else System.out.println("Map object is null");
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+		mapper.registerModule(new JavaTimeModule());
+		try {
+			//map = mapper.readValue(responseBody.getBytes(), new TypeReference<Map<String, Object>>() {});
+			promote = mapper.readValue(responseBody.getBytes(), PromotionsList.class);
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//if(promotion != null) System.out.println(promotion.toString() );
+		//List<Object> promoteListing = new ArrayList<Object>();
+		//model.addAllObjects(map);
+		model.addObject("Promotion", promote);
+		//System.out.println("Model map object"+ model.getModel().toString() );
+		//else System.out.println("Map object is null");
+		model.setViewName("PromoteView");
+		
+		
+		//model.setViewName("Promoteview");
+		//return "Test";
+		return model;
 	}
-	
+
+	/*HttpHeaders headers = new HttpHeaders();
+	headers.setContentType(new MediaType(""));
+	HttpEntity<PromotionsList> entity = new HttpEntity<PromotionsList>(headers);*/
+	//PromotionsList promotion = new PromotionsList();
+	//if(responseBody != null || !responseBody.isBlank()) System.out.println("Promotion details:"+responseBody.toString());
+	//else System.out.println("Promotion details is null");
+	/*HttpStatus statusCode = response.getStatusCode();
+	if(statusCode == HttpStatus.OK) 
+	{
+		PromotionsList promote = response.getBody();
+		model.addAttribute("Promotion", promote);
+		if(promote != null) System.out.println(promote.toString() );
+	}*/
+	//Object promotionResponse = resttemp.getForObject(resourceUrl, Object.class);
+	//Map<String, Object> map = resttemp.getForObject(resourceUrl, HashMap.class) ;
 }
---
+++ src/main/java/com/group1/dto/PromotionsList.java
@@ -0,0 +1,157 @@
+package com.group1.dto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class PromotionsList implements Serializable{
+
+	private Integer promoteCodeID;
+	
+	private Set<Object> billsWithPromoteID;
+	
+	private String promoteCodeName;
+	
+	private String promoteCodeDescription;
+
+	private Integer discountPercent;
+
+	private Integer discountMaxAmount;
+
+	private LocalDateTime startDate;
+
+	private LocalDateTime endDate;
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+	private LocalDateTime startDateInput;
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+	private LocalDateTime endDateInput;
+
+	private Boolean enabled;
+	
+	public PromotionsList() {
+		
+	}
+
+	public PromotionsList(Integer promoteCodeID, Set<Object> billsWithPromoteID, String promoteCodeName,
+			String promoteCodeDescription, Integer discountPercent, Integer discountMaxAmount, LocalDateTime startDate,
+			LocalDateTime endDate, LocalDateTime startDateInput, LocalDateTime endDateInput, Boolean enabled) {
+		super();
+		this.promoteCodeID = promoteCodeID;
+		this.billsWithPromoteID = billsWithPromoteID;
+		this.promoteCodeName = promoteCodeName;
+		this.promoteCodeDescription = promoteCodeDescription;
+		this.discountPercent = discountPercent;
+		this.discountMaxAmount = discountMaxAmount;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.startDateInput = startDateInput;
+		this.endDateInput = endDateInput;
+		this.enabled = enabled;
+	}
+
+	public Integer getPromoteCodeID() {
+		return promoteCodeID;
+	}
+
+	public void setPromoteCodeID(Integer promoteCodeID) {
+		this.promoteCodeID = promoteCodeID;
+	}
+
+	public Set<Object> getBillsWithPromoteID() {
+		return billsWithPromoteID;
+	}
+
+	public void setBillsWithPromoteID(Set<Object> billsWithPromoteID) {
+		this.billsWithPromoteID = billsWithPromoteID;
+	}
+
+	public String getPromoteCodeName() {
+		return promoteCodeName;
+	}
+
+	public void setPromoteCodeName(String promoteCodeName) {
+		this.promoteCodeName = promoteCodeName;
+	}
+
+	public String getPromoteCodeDescription() {
+		return promoteCodeDescription;
+	}
+
+	public void setPromoteCodeDescription(String promoteCodeDescription) {
+		this.promoteCodeDescription = promoteCodeDescription;
+	}
+
+	public Integer getDiscountPercent() {
+		return discountPercent;
+	}
+
+	public void setDiscountPercent(Integer discountPercent) {
+		this.discountPercent = discountPercent;
+	}
+
+	public Integer getDiscountMaxAmount() {
+		return discountMaxAmount;
+	}
+
+	public void setDiscountMaxAmount(Integer discountMaxAmount) {
+		this.discountMaxAmount = discountMaxAmount;
+	}
+
+	public LocalDateTime getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDateTime startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDateTime getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDateTime endDate) {
+		this.endDate = endDate;
+	}
+
+	public LocalDateTime getStartDateInput() {
+		return startDateInput;
+	}
+
+	public void setStartDateInput(LocalDateTime startDateInput) {
+		this.startDateInput = startDateInput;
+	}
+
+	public LocalDateTime getEndDateInput() {
+		return endDateInput;
+	}
+
+	public void setEndDateInput(LocalDateTime endDateInput) {
+		this.endDateInput = endDateInput;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	public String toString() {
+		return "promoteCodeID=" + promoteCodeID + "\n       billsWithPromoteID=" + billsWithPromoteID
+				+ "\n       promoteCodeName=" + promoteCodeName + "\n       promoteCodeDescription="
+				+ promoteCodeDescription + "\n       discountPercent=" + discountPercent + "\n       discountMaxAmount="
+				+ discountMaxAmount + "\n       startDate=" + startDate + "\n       endDate=" + endDate
+				+ "\n       startDateInput=" + startDateInput + "\n       endDateInput=" + endDateInput
+				+ "\n       enabled=" + enabled;
+	}
+
+	
+}
